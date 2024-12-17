5aa22f3f2c1a3452d5554d4b7d5394a2a72929db
Legolas2222 <pa.struzyna@gmail.com>
2024-12-17 22:48:20 +0100

    Added a test controller for the teachers

---
+++ src/main/java/de/das_rsg/cs_workshop/java_app/controller/TeacherController.java
@@ -0,0 +1,28 @@
+package de.das_rsg.cs_workshop.java_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/lehrer")
+@RestController
+public class TeacherController {
+
+    private String[] lehrer;
+    public TeacherController() {
+        this.lehrer = new String[] {"SBK", "TES", "ZAP"};
+    }
+
+    @GetMapping
+    public String testLehrer(@RequestParam int index) {
+        if (index == 0) {
+            return lehrer[0];
+        }
+        if (index - 1 == lehrer.length) {
+            return lehrer[index - 1];
+        } else {
+            return "Lehrer nicht gefunden";
+            
+        }
+    } 
+}
