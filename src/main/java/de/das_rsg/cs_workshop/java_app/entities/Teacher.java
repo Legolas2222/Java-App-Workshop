f0db8b70ce1f4b1e17520cb3a9306cfacaaeada7
Legolas2222 <95238358+Legolas2222@users.noreply.github.com>
2024-12-26 18:56:24 +0100

    Added some minor comments for better understanding

--- src/main/java/de/das_rsg/cs_workshop/java_app/entities/Teacher.java
+++ src/main/java/de/das_rsg/cs_workshop/java_app/entities/Teacher.java
@@ -2,6 +2,9 @@ package de.das_rsg.cs_workshop.java_app.entities;
 
 import de.das_rsg.cs_workshop.java_app.entities.core.Entity;
 
+// Wie ihr sehen könnt, erbt die Klasse Teacher von der Klasse Entity
+// Das bedeutet, dass die Klasse Teacher alle Methoden und Attribute von Entity übernimmt
+// Das ist in diesem Fall sinnvoll, da jede Entität eine eindeutige ID haben muss
 public class Teacher extends Entity {
     private String firstName;
     private String lastName;
--- src/main/java/de/das_rsg/cs_workshop/java_app/entities/core/Entity.java
+++ src/main/java/de/das_rsg/cs_workshop/java_app/entities/core/Entity.java
@@ -1,5 +1,6 @@
 package de.das_rsg.cs_workshop.java_app.entities.core;
 
+// Paket um UUIDs zu generieren
 import java.util.UUID;
 
 
@@ -7,6 +8,8 @@ public class Entity {
     private final String id;
 
     public Entity() {
+        // Legt fest, das jede Entität eine eindeutige UUID haben muss
+        // Eine UUID sieht ungefähr so aus: 550e8400-e29b-41d4-a716-446655440000
         this.id = UUID.randomUUID().toString();
     }
 
