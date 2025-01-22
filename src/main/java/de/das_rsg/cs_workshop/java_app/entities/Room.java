e214aca18cc2ad1baf416782c098cc312ae1d3c4
Benjamin <142577122+Benni2007@users.noreply.github.com>
2025-01-22 09:49:24 +0100

    Update Room.java

--- src/main/java/de/das_rsg/cs_workshop/java_app/entities/Room.java
+++ src/main/java/de/das_rsg/cs_workshop/java_app/entities/Room.java
@@ -4,14 +4,17 @@ import de.das_rsg.cs_workshop.java_app.entities.core.Entity;
 
 public class Room extends Entity{
 
-    private int roomnumber; 
+    private int roomnumber;
+    private int kapazitaet;
 
     public Room(){
         super();
     }
 
-    public Room(int rn){
+    public Room(int rn, int k){
         super();
+        roomnumber = rn;
+        kapazitaet = k;
     }
 
     public int getRoomnumber(){
@@ -20,6 +23,14 @@ public class Room extends Entity{
 
     public void setRoomnumber(int rn){
         roomnumberm = rn;
-    }    
+    }
+
+    public int getKapazitaet(){
+        return kapazitaet;
+    }
+
+    public void setKapazitaet(int k){
+        kapazitaet = k;
+    }
 
 }
